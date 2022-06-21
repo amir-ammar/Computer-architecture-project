34ad3127200d9ebd43815e83193f8783ed38a4b3
amir-ammar <am7099533@gmail.com>
2022-06-21 23:26:17 +0200

    add: program files

---
+++ src/main/java/mips/Utils/Observer.java
@@ -0,0 +1,10 @@
+package mips.Utils;
+
+public interface Observer {
+
+    // this is for the clk triggerd components
+    public void update();
+    // this is for the compientional components
+    public void update(int data);
+
+}
